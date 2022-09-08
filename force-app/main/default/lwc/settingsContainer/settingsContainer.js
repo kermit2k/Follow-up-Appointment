c403d2e6068329742c03d424f57ba41263db69a0
Inbal <ibenitamar@salesforce.com>
2022-09-08 15:26:02 +0300

    Added properties to settings component

--- force-app/main/default/lwc/settingsContainer/settingsContainer.js
+++ force-app/main/default/lwc/settingsContainer/settingsContainer.js
@@ -1,5 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class SettingsContainer extends LightningElement {
     title="Hello Settings LWC"
+
+    //@api serviceAppointmentId;
+    @api operatingHoursId = [string];
+    @api schedulingPolicyId =[string];
+    @api arrivalWindowMethod = [string];
+    @api schedulingHorizonUnit = [string];
+    @api sechedulingHorizonValue = [int];
+    @api enableAssignToMe = [boolean];
+    @api enableAssignToEveryone = [boolean];
+
+    _recordId;
+
+    @api set recordId(recordId) {
+        if (recordId !== this._recordId) {
+            this._recordId = recordId;
+        }
+    }
+
+    get recordId() {
+        return this._recordId;
+    }
 }

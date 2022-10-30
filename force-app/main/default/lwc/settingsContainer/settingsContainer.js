84876d909e2da169d9da333a3257c3c367546772
Inbal <ibenitamar@salesforce.com>
2022-10-30 14:43:32 +0200

    assign to me apex functions + settings and states

--- force-app/main/default/lwc/settingsContainer/settingsContainer.js
+++ force-app/main/default/lwc/settingsContainer/settingsContainer.js
@@ -1,24 +1,26 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 import ID_FIELD from '@salesforce/schema/ServiceAppointment.Id';
 import WORK_TYPE_FIELD from '@salesforce/schema/ServiceAppointment.WorkTypeId';
 import SCHED_END_FIELD from '@salesforce/schema/ServiceAppointment.SchedEndTime';
 import SCHED_START_FIELD from '@salesforce/schema/ServiceAppointment.SchedStartTime';
 import APPOINTMENT_NUMBER_FIELD from '@salesforce/schema/ServiceAppointment.AppointmentNumber';
 import DURATION from '@salesforce/schema/ServiceAppointment.Duration';
-
-
+//import getUserInfo from '@salesforce/apex/UserDetails.getUserInfo';
+import Id from '@salesforce/user/Id';
 
 export default class SettingsContainer extends LightningElement {
     title="Hello Settings LWC"
 
     //@api serviceAppointmentId;
+    userId = Id;
     @api operatingHoursId = "0OH7e000000Cj5iGAC";
     @api schedulingPolicyId ="a0Z7e00000MImdzEAD";
     @api arrivalWindowMethod = "Arrival Windows";
     @api schedulingHorizonUnit = "Days";
     @api sechedulingHorizonValue = "20";
+    enableAssignTo;
     @api enableAssignToMe;
-    @api enableAssignToEveryone;
+    @api enableAssignToEveryAvailable;
     @api showExactArrivalTime =  false;
     recommendedScore = 80;
     useDefaultFields = true;
@@ -43,4 +45,22 @@ export default class SettingsContainer extends LightningElement {
     get recordId() {
         return this._serviceAppointmentId;
     }
+
+    connectedCallback(){
+        
+        this.enableAssignToMe = true;
+        this.enableAssignToEveryAvailable = true;
+    }
+
+
+    /*@wire(getUserInfo, { userId: Id }) 
+    userDetails({error, data}) {
+        if (data) {
+            console.log("USer Id:::" + JSON.stringify(data));
+        } else if (error) {
+            this.error = error ;
+        }
+    }*/
+
+    
 }

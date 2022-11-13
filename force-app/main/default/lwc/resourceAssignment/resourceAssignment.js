9dd34063d23d3dd506451e4e24a3614b3abe45cd
Inbal <ibenitamar@salesforce.com>
2022-11-13 11:52:32 +0200

    assign to me lables

--- force-app/main/default/lwc/resourceAssignment/resourceAssignment.js
+++ force-app/main/default/lwc/resourceAssignment/resourceAssignment.js
@@ -1,22 +1,27 @@
 import { LightningElement, api } from 'lwc';
+import customLabels from './labels';
 
 export default class ResourceAssignment extends LightningElement {
+    LABELS = customLabels;
     value = 'ASSIGN_TO_ME';
     @api currentAssignmentMethod;
     @api userName="";
-    resourceAssignmentTitle ="Mobile Worker";
+    resourceAssignmentTitle = this.LABELS.Appointment_ReBooking_Mobile_Worker_radio_title;
     @api selecteddate;
     @api isExcluded;
-    @api excludedMsg = "Excluded msg..";
+    @api excludedMsg = this.LABELS.Appointment_ReBooking_cant_select_Mobile_Worker_excluded;
 
     @api get showExcludedMsg(){
         return this.isExcluded;
     }
 
+    
+    
+
     get options() {
         return [
-            { label: `You (${this.userName})`, value: 'ASSIGN_TO_ME' },
-            { label: 'Any available worker', value: 'ASSIGN_TO_ANY_AVIALABLE' },
+            { label: `${this.LABELS.Appointment_ReBooking_Mobile_Worker_radio_assignToMe_lable} (${this.userName})`, value: 'ASSIGN_TO_ME' },
+            { label: this.LABELS.Appointment_ReBooking_Mobile_Worker_radio_assignToAnyAvailable_lable, value: 'ASSIGN_TO_ANY_AVIALABLE' },
         ];
     }
 
